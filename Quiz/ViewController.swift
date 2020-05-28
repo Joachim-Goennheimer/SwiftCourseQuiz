71d948d5e5dcbff8a2488d4d939867676d08c3e0
Joachim Goennheimer <Joachim.goennheimer@ibm.com>
2020-05-28 14:31:09 +0200

    implement swap question labels

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -42,10 +42,19 @@ class ViewController: UIViewController {
 //            self.questionLabel.alpha = 1
 //        }
         
-        UIView.animate(withDuration: 0.5, animations: {
-//            self.questionLabel.alpha = 1
+//        UIView.animate(withDuration: 0.5, animations: {
+////            self.questionLabel.alpha = 1
+//            self.currentQuestionLabel.alpha = 0
+//            self.nextQuestionLabel.alpha = 1
+//        })
+        
+        UIView.animate(withDuration: 0.5, delay: 0, options: [], animations: {
             self.currentQuestionLabel.alpha = 0
             self.nextQuestionLabel.alpha = 1
+        },
+                       completion: {_ in
+                        swap(&self.currentQuestionLabel,
+                             &self.nextQuestionLabel)
         })
     }
     
